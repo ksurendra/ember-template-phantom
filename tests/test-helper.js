a1e97b141660090113f742a0e99d87191ef2e486
Suren Konathala <suren@Surens-MacBook-Pro.local>
2022-01-24 13:23:26 -0600

    initial working version

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'ember-template-phantom/app';
+import config from 'ember-template-phantom/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
